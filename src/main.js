b78a60eb542cda8d4a4938e5c3eca72d71b0af31
AndrewYakovlev <gmde@ya.ru>
2022-10-08 14:58:15 +0300

    Vuex error fixed

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
 import Vue from 'vue'
 import router from './router'
 import App from '../src/App'
-
 import store from './store/main'
+
 import Vuex from 'vuex'
 import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
-Vue.use(Vuex)
+
 
 Vue.prototype.axios = axios
 Vue.prototype.$store = store
