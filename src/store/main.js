41f46586a8d6802f887175e3f4de14d74ddf3a2e
AndrewYakovlev <gmde@ya.ru>
2022-10-08 14:58:47 +0300

    Comment for fixed Vuex error

--- src/store/main.js
+++ src/store/main.js
@@ -1,7 +1,10 @@
+// Импортируем Vue здесь, если вызов стора произойдет раньше
+
 import Vue from "vue";
 import Vuex from 'vuex'
 import createPersistedState from "vuex-persistedstate";
 
+// И просим vue использовать vuex
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
