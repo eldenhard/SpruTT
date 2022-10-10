0da71c76a41d5f88c4135def1394753613b1cf8e
eldenhard <elden.hard@mail.ru>
2022-10-10 19:59:22 +0300

    sdfsdf

---
+++ src/components/notifications/NotificationAlert.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="wrapper-success" id="notifications-2" style="display: none;">
+<div class="card">
+    <div class="subject">
+    <h5>Ошибка авторизации</h5>
+    <p>Пожалуйста, проверьте ваши введенные данные</p>
+    </div>
+</div>
+</div>
+</template>
+<style>
+
+</style>
--- src/components/notifications/Notifications.vue
+++ src/components/notifications/Notifications.vue
@@ -9,14 +9,7 @@
 </div>
 </div>
 
-<div class="wrapper-success" id="notifications-2" style="display: none;">
-<div class="card">
-    <div class="subject">
-    <h5>Ошибка авторизации</h5>
-    <p>Пожалуйста, проверьте ваши введенные данные</p>
-    </div>
-</div>
-</div>
+
     </div>
 </template>
 
