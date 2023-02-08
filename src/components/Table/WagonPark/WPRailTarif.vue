8e15bcdf4d72ee9d7414f258358aff118dc86d24
eldenhard <reshetilo-1997@mail.ru>
2023-02-08 16:41:08 +0300

    доделанное верхнее меню

--- src/components/Table/WagonPark/WPRailTarif.vue
+++ src/components/Table/WagonPark/WPRailTarif.vue
@@ -61,24 +61,24 @@
     <div class="railtarif__price">
       <div class="railtarif__predata">
         <div>
-          <p>Тип отправки: <span class="pretext">{{ speed }}</span></p>
+          <p>Тип отправки: <span class="pretext">{{ category }}</span></p>
           <p>Скорость: <span class="pretext">{{ speed }}</span></p>
         </div>
         <div>
-          <p>Код ЕСТНГ: <span class="pretext">{{ speed }}</span></p>
-          <p>Вес: <span class="pretext">{{ speed }}</span></p>
+          <p>Код ЕСТНГ: <span class="pretext">{{ code_estng }}</span></p>
+          <p>Вес: <span class="pretext">{{ weight }}</span></p>
         </div>
         <div>
-          <p>Код ГНГ: <span class="pretext">{{ speed }}</span></p>
-          <p>Тип вагона: <span class="pretext">{{ speed }}</span></p>
+          <p>Код ГНГ: <span class="pretext">{{ code_gng }}</span></p>
+          <p>Тип вагона: <span class="pretext">{{ wagon_type }}</span></p>
         </div>
         <div>
-          <p>Принадлежность: <span class="pretext">{{ speed }}</span></p>
-          <p>Количество: <span class="pretext">{{ speed }}</span></p>
+          <p>Принадлежность: <span class="pretext">{{ belong_in }}</span></p>
+          <p>Количество: <span class="pretext">{{ amount }}</span></p>
         </div>
       </div>
       <div v-show="first" class="slider">
-        <input class="textarea" placeholder="Тип отправки" type="text" /> <br />
+        <input class="textarea" placeholder="Тип отправки" type="text" v-model="category" /> <br />
         <input
           class="textarea"
           placeholder="Скорость"
@@ -90,9 +90,9 @@
         </div>
       </div>
       <div v-show="second" class="slider">
-        <input class="textarea" placeholder="Код ЕСТНГ" type="text" /><br />
-        <input class="textarea" placeholder="Вес" type="text" /><br />
-        <input class="textarea" placeholder="Код ГНГ" type="text" /><br />
+        <input class="textarea"  v-model="code_estng" placeholder="Код ЕСТНГ" type="text" /><br />
+        <input class="textarea" v-model="weight" placeholder="Вес" type="text" /><br />
+        <input class="textarea" v-model="code_gng" placeholder="Код ГНГ" type="text" /><br />
         <div
           class="btn-group"
           style="justify-content: space-between !important"
@@ -102,10 +102,10 @@
         </div>
       </div>
       <div v-show="third" class="slider">
-        <input class="textarea" placeholder="Тип вагона" type="text" /> <br />
-        <input class="textarea" placeholder="Принадлежность" type="text" />
+        <input class="textarea" placeholder="Тип вагона" type="text" v-model="wagon_type" /> <br />
+        <input class="textarea" placeholder="Принадлежность" type="text" v-model="belong_in" />
         <br />
-        <input class="textarea" placeholder="Количество" type="text" />
+        <input class="textarea" placeholder="Количество" type="text" v-model="amount" />
         <div
           class="btn-group"
           style="justify-content: space-between !important"
@@ -223,6 +223,12 @@ export default {
       stations: [],
       category: "",
       speed: "",
+      code_estng: "",
+      weight: "",
+      code_gng: "",
+      wagon_type: "",
+      belong_in: "",
+      amount: "",
       first: true,
       second: false,
       third: false,
