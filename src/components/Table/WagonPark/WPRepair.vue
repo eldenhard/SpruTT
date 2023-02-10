a312628ad93c56fdcda4d3b3dd52848adc787e7b
eldenhard <reshetilo-1997@mail.ru>
2023-02-10 14:51:35 +0300

    ремонты

--- src/components/Table/WagonPark/WPRepair.vue
+++ src/components/Table/WagonPark/WPRepair.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <Loader :loader="loader" />
+    <p class="explanation"> * Для просмотра изменений по вагону кликните на строку с этим вагоном</p>
+       
     <table class="table-bordered table-sm">
       <thead>
         <tr>
@@ -61,16 +63,76 @@
                 <td>{{ rep.axis8_left }}</td>
                 <td>{{ rep.axis8_right }}</td>
            
-          <td>{{ rep.created_at }}</td>
+          <td>{{ rep.created_at.slice(0,10) }}</td>
         </tr>
       </tbody>
     </table>
     <b-modal ref="ModalHistoryWagon" hide-footer title="Последние изменения">
-      <div class="bg" v-for="data in history_data" :key="data.id">
-        <h3>{{ data.wagon }}</h3>
+      <div class="bg">
+        <table class="table-bordered table-sm">
+      <thead>
+        <tr>
+          <th colspan="1">Номер вагона</th>
+          <th colspan="2">Ось 1
+            <td class="border-none">Л</td>
+            <td class="border-none">П</td>
+          </th>
+          <th colspan="2">Ось 2
+            <td class="border-none">Л</td>
+            <td class="border-none">П</td>
+          </th>
+          <th colspan="2">Ось 3
+            <td class="border-none">Л</td>
+            <td class="border-none">П</td>
+          </th>
+          <th colspan="2">Ось 4
+            <td class="border-none">Л</td>
+            <td class="border-none">П</td>
+          </th>
+          <th colspan="2">Ось 5
+            <td class="border-none">Л</td>
+            <td class="border-none">П</td>
+          </th>
+          <th colspan="2">Ось 6
+            <td class="border-none">Л</td>
+            <td class="border-none">П</td>
+          </th>
+          <th colspan="2">Ось 7
+            <td class="border-none">Л</td>
+            <td class="border-none">П</td>
+          </th>
+          <th colspan="2">Ось 8
+            <td class="border-none">Л</td>
+            <td class="border-none">П</td>
+          </th>
+          <th colspan="1">Дата</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="data in history_data" :key="data.id" @click="openInform(data.wagon)">
+            <td>{{ data.wagon }}</td>
 
-
-<h1>Данные</h1>
+                <td>{{ data.axis1_left }}</td>
+                <td>{{ data.axis1_right }}</td>
+                <td>{{ data.axis2_left }}</td>
+                <td>{{ data.axis2_right }}</td>
+                <td>{{ data.axis3_left }}</td>
+                <td>{{ data.axis3_right }}</td>
+                 <td>{{ data.axis4_left }}</td>
+                 <td>{{ data.axis4_right }}</td>
+                <td>{{ data.axis5_left }}</td>
+                <td>{{ data.axis5_right }}</td>
+                <td>{{ data.axis6_left }}</td>
+                <td>{{ data.axis6_right }}</td>
+                <td>{{ data.axis7_left }}</td>
+                <td>{{ data.axis7_right }}</td>
+                <td>{{ data.axis8_left }}</td>
+                <td>{{ data.axis8_right }}</td>
+           
+          <td >{{ (data.created_at).slice(0,10) }}</td>
+        </tr>
+      </tbody>
+    </table>
       </div>
 
       <b-button class="mt-3" variant="outline-danger" block @click="hideModal">Закрыть</b-button>
@@ -113,12 +175,19 @@ export default {
       });
   },
   methods: {
-    openInform(wagon){
-        api.getWagonRepairHistory(wagon)
-        .then(response => {
-            this.history_data = response.data
-            this.showModal()
+    openInform(wagon) {
+      this.loader = true;
+      api
+        .getWagonRepairHistory(wagon)
+        .then((response) => {
+          this.history_data = response.data;
+          console.log(this.history_data);
+          this.showModal();
+          this.loader = false;
         })
+        .catch((error) => {
+          this.loader = false;
+        });
     },
     showModal() {
       this.$refs["ModalHistoryWagon"].show();
@@ -129,15 +198,38 @@ export default {
     toggleModal() {
       this.$refs["ModalHistoryWagon"].toggle("#toggle-btn");
     },
-  }
+  },
 };
 </script>
 
 <style scoped>
-.border-none{
-    border: none !important
+.explanation {
+  font-size: 13px;
+  color: grey;
 }
-tr:hover{
-    background: lightgray;
+.border-none {
+  border: none !important;
+}
+tr:hover {
+  background: lightgray;
+}
+.content-history {
+  display: flex;
+}
+.textarea {
+  background: white;
+  border: 1px solid grey !important;
+  margin-top: 1%;
+}
+.bg {
+  background: white;
+  -webkit-box-shadow: 3px 4px 8px 0px rgba(34, 60, 80, 0.2);
+  -moz-box-shadow: 3px 4px 8px 0px rgba(34, 60, 80, 0.2);
+  box-shadow: 3px 4px 8px 0px rgba(34, 60, 80, 0.2);
+}
+.created-at {
+  color: rgb(146, 146, 146);
+  margin: 2% 2% 0 0;
+  text-align: right;
 }
 </style>
