0dacaaf6494b653025feb27d31fa1b85cc8349e8
eldenhard <reshetilo-1997@mail.ru>
2023-05-12 17:23:06 +0300

    ставка по аренде

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,28 @@
+
+<!-- <template>
+  <div id="navbarMain">
+    <b-card no-body>
+      <b-tabs pills card vertical nav-wrapper-class="w-100" class="test" style="background: white;height: 100vh !important;text-align:center !important;
+            justify-content: center !important;">
+        <b-container class="bv-example-row">
+          <b-tab title="Вагоны" @click="newTab('Вагоны')" style="display: none !important"> </b-tab>
+          <b-tab title="Телеграммы" @click="newTab('Телеграммы')" style="display: none !important"> </b-tab>
+          <b-tab title="Ремонты" @click="newTab('Ремонты')" style="display: none !important"> </b-tab>
+          <b-tab title="Расчет" @click="newTab('Расчет')" style="display: none !important"> </b-tab>
+          <b-tab  title="Отправки БЧ" @click="newTab('Отправки БЧ')" style="display: none !important"> </b-tab>
+          <b-tab  title="Тариф по сопр. терр." @click="newTab('Тариф по сопредельным территориям')" style="display: none !important"> </b-tab>
+
+        </b-container>
+      </b-tabs>
+    </b-card>
+  </div>
+</template> -->
+
+
+
 <template>
-
-
-  <b-card no-body style="margin-top: 1%">
+  
+<b-card no-body style="margin-top: 1%">
     <b-tabs pills card vertical style="background: white">
       <b-container >
         <b-tab title="Справочник контрагенты">
@@ -9,12 +30,16 @@
                     <partnerTable/>
               </b-card-text>
               </b-tab>
-            <b-tab title="Справочник договора" active>
+            <b-tab title="Справочник договора">
               <b-card-text>
                 <contractTable/>
               </b-card-text>
             </b-tab>
-                
+            <b-tab title="Ставки по аренде" active>
+                <b-card-text>
+                    <rentalrateTable/>
+              </b-card-text>
+              </b-tab>
           </b-container>
 
     </b-tabs>
@@ -23,15 +48,39 @@
 </template>
 
 <style scoped>
+select {
+  width: 100%;
+  box-sizing: border-box;
+}
 
+#navbarMain {
+  background: #ffffff;
+  display: flex;
+  flex-direction: column;
+  height: 100vh !important;
+}
+
+.nav .nav-pills {
+  height: 100vh !important;
+}
+
+.nav-item {
+  text-align: center !important;
+}
+
+li.nav-item {
+  text-align: center !important;
+  justify-content: center !important;
+}
 </style>
 
 <script>
 import partnerTable from '@/components/Table/PartnerTable.vue'
 import contractTable from '@/components/Table/ContractTable.vue'
+import rentalrateTable from '@/components/Table/RentalRateTable.vue'
 export default{
     name: 'Directory',
-    components: {partnerTable, contractTable },
+    components: {partnerTable, contractTable , rentalrateTable},
     mounted(){
       document.title = 'Справочники'
 
