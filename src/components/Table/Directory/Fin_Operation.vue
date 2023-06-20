8dd13bd38c98c0983a0cb7302b4f0da9e8d2e1e3
eldenhard <elden.hard@mail.ru>
2023-06-20 20:45:09 +0300

    a

--- src/components/Table/Directory/Fin_Operation.vue
+++ src/components/Table/Directory/Fin_Operation.vue
@@ -9,15 +9,24 @@
     </div>
 
 
-      <table border="1"  style="margin-top: 15%;">
-        <th rowspan="2">Наименование статьи</th>
-        <th rowspan="2">План утв.</th>
-        <template v-for="item in num">
+    <table border="1" style="margin-top: 3%;">
+      <thead>
+        <tr>
+          <th rowspan="2">Наименование статьи</th>
+          <th rowspan="2">План утв.</th>
+          <template v-for="item in num">
             <th :key="item.id">{{ item }}</th>
+          </template>
+        </tr>
+        <template v-for="i in send_data" >
+          <th :key="i.id" :class="{weekend: isWeekend(i)}">{{ i }}</th>
         </template>
-   
-      </table>
-    
+
+      </thead>
+
+
+    </table>
+
   </div>
 </template>
 
@@ -28,31 +37,64 @@ export default {
     return {
       month: "",
       num: "",
- 
+      send_data: "",
+
     };
   },
-  methods: {
-    getDayOnMonth(){
-        let days = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
-        let arr = this.month.split('-')
-        let lastday = new Date(arr[0], arr[1], 0)
-        this.num =  lastday.getDate()
 
+  methods: {
+    isWeekend(data){
+      if(data == 'ВС' || data == 'СБ'){
+        return true
+      } return false
+    },
+    getDayOnMonth() {
+      let arr = this.month.split('-')
+      let lastday = new Date(arr[0], arr[1], 0)
+      this.num = lastday.getDate()
+
+    let num = lastday.getDate()
+    let array = []
+    for(let i = 1; i<=num; i++){
+      if(i <= 9){
+        array.push(`${this.month}-0${i}`)
+      }else {
+        array.push(`${this.month}-${i}`)
+      }
+     
     }
-    
+     let days = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+    let data = array.map(item => {
+      return new Date(item)
+    })
+    let send_data = data.map(item => {
+      return days[item.getDay()]
+    })
+    this.send_data = send_data
   }
+    },
+
+
+  
 };
 </script>
 
 <style lang="scss" scoped>
 .month_block {
-margin-left: auto;
-display: flex;
-flex-direction: column;
-justify-content: flex-end;
-    button{
-        width: 25%
-    }
-}
+  margin-left: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
 
+  button {
+    width: 25%
+  }
+}
+th{
+  font-weight:  400 !important;
+  font-size: 13px !important;
+}
+.weekend{
+  color: #F18A46;
+}
 </style>
