8d084d413138ef642c6ebd19c36d3cc027ec50b3
eldenhard <elden.hard@mail.ru>
2024-04-15 17:45:55 +0300

    a

--- src/App.vue
+++ src/App.vue
@@ -69,17 +69,38 @@ export default {
   },
   // Синхронизация
   async mounted() {
+
+    const cashe = await cashe.open()
+    console.log(cashe)
+    // for(let i = 0; i < 15; i++){
+    //   await fetch('https://jsonplaceholder.typicode.com/comments')
+    //   .then(response => response.json())
+    //   .then(json => console.log(json))
+    // }
+  
+
+
     let id_rocket = JSON.parse(localStorage.getItem('vuex')).auth.uid
 
     let { first_name, last_name, email } = JSON.parse(localStorage.getItem('vuex')).auth.user.user
-    console.log(first_name, last_name)
-    console.log(id_rocket)
     LogRocket.init('fs2mx3/sprutt')
     LogRocket.identify(id_rocket, {
       name: `${first_name} ${last_name}`,
       email: `${email}`,
     });
-    
+
+
+    if (navigator.storage && navigator.storage.estimate) {
+      const quota = await navigator.storage.estimate();
+      // quota.usage -> свободное пространство ( в байтах).
+      // quota.quota -> доступное пространство (в байтах).
+      const percentageUsed = (quota.usage / quota.quota) * 100;
+      console.log(`Вы используете ${percentageUsed}% от доступного пространства.`);
+      const remaining = quota.quota - quota.usage;
+      console.log(`Вы можете записать еще ${remaining} байт.`);
+    }
+
+
     this.loader_mini = true
     let objStation = {
       'ОКТЯБРЬСКАЯ': 'ОКТ',
@@ -132,19 +153,19 @@ export default {
     // console.log(this.token)
     try {
       localStorage.setItem('accessToken', JSON.stringify(this.token))
-      if (!window.location.href.includes('fin_operation')) {
-        await Promise.all([
-          this.$store.dispatch(actionTypes.getStaffGroups),
-          this.$store.dispatch('getClient'),
-          this.$store.dispatch('getCounterpartie'),
-          this.$store.dispatch('getRoadAsRoad'),
+      // if (!window.location.href.includes('fin_operation')) {
+      //   await Promise.all([
+      //     this.$store.dispatch(actionTypes.getStaffGroups),
+      //     this.$store.dispatch('getClient'),
+      //     this.$store.dispatch('getCounterpartie'),
+      //     this.$store.dispatch('getRoadAsRoad'),
 
-          this.$store.dispatch(actionTypes.staffGlobal),
-          this.$store.dispatch(cpActionTypes.getCounterparties, { url: 'personal/counterparties/?page_size=500', clear: true }),
-          this.$store.dispatch(userActionTypes.getUsers, { url: 'personal/users/?page_size=500', clear: true }),
-          this.$store.dispatch(ccActionTypes.getCargoCode, { url: 'wagon-park/cargo/?page_size=500', clear: true }),
-        ])
-      }
+      //     this.$store.dispatch(actionTypes.staffGlobal),
+      //     this.$store.dispatch(cpActionTypes.getCounterparties, { url: 'personal/counterparties/?page_size=500', clear: true }),
+      //     this.$store.dispatch(userActionTypes.getUsers, { url: 'personal/users/?page_size=500', clear: true }),
+      //     this.$store.dispatch(ccActionTypes.getCargoCode, { url: 'wagon-park/cargo/?page_size=500', clear: true }),
+      //   ])
+      // }
     }
     catch (error) {
       console.error(error)
