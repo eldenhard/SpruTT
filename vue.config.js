34e4a2d21c324f4290a29d3d1bfd8c6d9f3e115a
[DAlexey <elden.hard@mail.ru>
2024-09-03 15:03:38 +0300

    настройка webpack

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,26 @@
 const { defineConfig } = require('@vue/cli-service')
+const TerserPlugin = require('terser-webpack-plugin') // для минификации JavaScript
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  // Настройка Webpack
+  configureWebpack: {
+    optimization: {
+      minimize: true, // минимизация удаляются лишние пробелы и символы, переменные становятся короче, а записи сокращаются,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true, // удаляем все console.log
+              drop_debugger: true
+            },
+            output: {
+              comments: false // удаляем комментарии
+            }
+          }
+        })
+      ]
+    }
+  }
   
 })
