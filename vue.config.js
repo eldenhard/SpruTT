fc302366100edcc82e8663397cffd289e4cc5017
[DAlexey <elden.hard@mail.ru>
2024-09-03 16:16:48 +0300

    webpack conf v5

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,20 @@
 const { defineConfig } = require('@vue/cli-service');
-const TerserPlugin = require('terser-webpack-plugin');
 
+const TerserPlugin = require('terser-webpack-plugin')
 module.exports = defineConfig({
   transpileDependencies: true,
 
   configureWebpack: {
     optimization: {
-      minimize: true,
       minimizer: [
         new TerserPlugin({
           terserOptions: {
             compress: {
-              drop_console: true, // Удаляет console.log
-              drop_debugger: true,
-            },
-            output: {
-              comments: false,
-            },
-          },
-          extractComments: false, // Удаляет комментарии из финальных файлов
-        }),
-      ],
-    },
+              drop_console: true
+            }
+          }
+        })
+      ]
+    }
   },
 });
