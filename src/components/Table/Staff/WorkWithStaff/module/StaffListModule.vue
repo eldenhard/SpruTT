b6306918a28326faa504cab5813d06e6e13cb74b
[DAlexey <elden.hard@mail.ru>
2024-10-11 15:01:42 +0300

    орг структура

--- src/components/Table/Staff/WorkWithStaff/module/StaffListModule.vue
+++ src/components/Table/Staff/WorkWithStaff/module/StaffListModule.vue
@@ -8,7 +8,12 @@ export default {
   data() {
     return {
       state: "",
+      employees: [],
     };
+  },
+  async mounted() {
+    let employees = await api.getAllStaff({ page_size: 500 });
+    this.employees = employees.data.data;
   },
   methods: {
     updateStaff(value) {
@@ -31,12 +36,12 @@ export default {
       <b-card no-body>
         <b-tabs card>
           <b-tab title="Организационная структура" active>
-            <b-card-text>
-              <OrganizationalStructureModule/>
+            <b-card-text style="margin-left: -8% !important; ">
+              <OrganizationalStructureModule :employees="employees"/>
             </b-card-text>
           </b-tab>
           <b-tab title="Временно отсутствующие сотрудники">
-            <b-card-text>Сотрудники временно отсутствующие по уважительной причине</b-card-text>
+            <b-card-text style="margin-left: -8% !important; ">Сотрудники временно отсутствующие по уважительной причине</b-card-text>
           </b-tab>
         </b-tabs>
       </b-card>
